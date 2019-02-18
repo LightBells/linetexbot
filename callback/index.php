88f29e69f42159c8b95c9011f5043b585087e0c2
LightBells <takahasi0928@gmail.com>
2019-02-18 14:05:56 +0900

    GoGO

--- callback/index.php
+++ callback/index.php
@@ -63,13 +63,12 @@ foreach($json['events'] as $e){
                 'content' => json_encode($body)
             )
         ));
+        try{
+            $result = file_get_contents("https://api.line.me/v2/bot/message/reply",false,$context);
+        }catch(Exception $e){
 
-        $result = file_get_contents("https://api.line.me/v2/bot/message/reply",false,$context);
-        
-        if (strpos($http_response_header[0], '400') === false) {
-        http_response_code(200);
-        error_log("Request failed: " . $result);
         }
+        
         if (strpos($http_response_header[0], '200') === false) {
         http_response_code(500);
         error_log("Request failed: " . $result);
